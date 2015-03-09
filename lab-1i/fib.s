ea35e72b198dee3edc15545a4ceae746043d3d1b
Veck Hsiao <fbukevin@gmail.com>
2015-03-09 16:47:48 +0000

    briefly finished lab1i

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,24 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	mov r3, r0			@ copy passed value
+	mov r0, #1 			@ result = 1 
+	subs r4, r4, #1			@ previous = -1	
+	mov r5, #0			@ sum = 0
+	mov r6, #0			@ i = 0	
+.Loop:
+	adds r5, r0, r4			@ sum = result + previous 
+	mov r4, r0			@ previous = result
+	mov r0, r5			@ result = sum
+	adds r6, r6, #1			@ i++
+	cmp r6, r3			@ if(i <= x)
+	ble .Loop			@ (i <= x) hold, continue loop
+	@bx lr				@ (i > x) hold, return
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
