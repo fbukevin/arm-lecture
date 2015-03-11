03f50f94c5c6fa6c1e251389153fc8049284d61b
Veck Hsiao <fbukevin@gmail.com>
2015-03-11 18:18:49 +0000

    add time consuming function in lab1 and lab1i

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,10 +7,18 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
+  clock_t start_t, end_t;
+  float total_t = 0;
 
   scanf("%d",&number);
+
+  start_t = clock();
   result = fibonacci(number);   
+  end_t = clock();
+  total_t = (float)(end_t - start_t)/CLOCKS_PER_SEC;
+
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  printf("Time Cost: %f seconds\n", total_t); 
 
   return 0;
 }
